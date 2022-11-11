0bca887650438f1b999a48fa9526de7e72ea5386
Santiago Japon <sajaponpa@uide.edu.ec>
2022-11-11 09:58:02 -0500

    nuevos ciclos for

--- Fundamentos/Ciclos repetitivos.py
+++ Fundamentos/Ciclos repetitivos.py
@@ -1,5 +1,5 @@
 #WHILE 
-print("WHILE")
+print("--------------------------WHILE--------------------------")
 i = 0
 
 while i <= 10:
@@ -8,7 +8,7 @@ while i <= 10:
     i += 1
 
 #WHILE BREAK
-print("BREAK")
+print("--------------------------BREAK----------------------")
 i = 0
 
 while i <= 10:
@@ -18,7 +18,7 @@ while i <= 10:
     i += 1
 
 #WHILE CONTINUE
-print("CONTINUE")
+print("------------------------CONTINUE----------------------------")
 i = 0
 
 while i <= 20:
@@ -34,14 +34,14 @@ Calcular los primeros 20 numeros pares, pero retirar el 10
 
 calcular los multiplos de 5 pero detener la ejecucion en el tercero
 """
-print("EJERCICIO 1")
+print("----------------------EJERCICIO 1----------------------")
 i = 1 
 
 while i <= 50:
     i += 2
     print(i)
 
-print("EJERCICIO 2")
+print("-----------------EJERCICIO 2---------------------")
 i = 0
 while i <=20:
     i += 2
@@ -49,10 +49,36 @@ while i <=20:
         continue
     print(i)
 
-print("EJERCICIO 3")
+print("------------------EJERCICIO 3--------------------")
 i = 5
 while i <= 30:
     print(i)
     if i == 15:
         break
     i += 5
+
+#FOR LOOP
+print("--------------FOR LOOP-----------------")
+personas =["Juan", "Pedro", "Maria"]
+for persona in personas:
+    print(persona)
+
+frutas = ["pera", "manzana", "banana", "fresas"]
+for fruta in frutas:
+    if fruta == "banana":
+        break
+    print(fruta)
+
+print("-----------------otro ejercicio----------")
+frutas = ["pera", "manzana", "banana", "fresas"]
+for fruta in frutas:
+    if fruta == "banana":
+        continue
+    print(fruta)
+
+#crear un ciclo for que realice el recorrido de 10 personas 
+
+print("------------------ejercicio en clase---------------------------")
+personas = ["Juan", "Pedro", "Mateo", "Carla", "Sofia", "Nathaly", "Charly","Daniel", "Emilio", "Santiago"]
+for persona in personas:
+    print(persona)
